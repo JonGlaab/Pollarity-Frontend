fb0a98806036b9c18d88cb31ae99d2eaf7fa86e8
ElizabethMokrusa <elimokrusa@msn.com>
2025-11-25 20:21:17 -0500

    Hero section on front page
    Added hero section to the front page. Decreased navbar padding.

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
         <AuthHandler />
         <Navbar />
         <Toaster />
-      <div className="App pt-20">
+      <div className="App pt-10">
         <Routes>
             <Route path="/" element={<UserRoute><Home /></UserRoute>} />
             <Route path="/register" element={<Register />} />
